695f16407f1e001ccd9732ae75660c554ebacbdc
Ryka Chandra <147470636+ryka-chandra@users.noreply.github.com>
2024-08-12 20:55:05 -0400

    Update FileLineIterator.java

--- FileLineIterator.java
+++ FileLineIterator.java
@@ -6,33 +6,14 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.util.NoSuchElementException;
 
-/**
- * FileLineIterator provides a useful wrapper around Java's provided
- * BufferedReader and provides practice with implementing an Iterator. Your
- * solution should not read the entire file into memory at once, instead reading
- * a line whenever the next() method is called.
- * <p>
- * Note: Any IOExceptions thrown by readers should be caught and handled
- * properly. Do not use the ready() method from BufferedReader.
- */
 public class FileLineIterator implements Iterator<String> {
 
-    // Add the fields needed to implement your FileLineIterator
     private BufferedReader bufferedReader;
     private boolean next;
     private String nextLine;
 
     /**
-     * Creates a FileLineIterator for the reader. Fill out the constructor so
-     * that a user can instantiate a FileLineIterator. Feel free to create and
-     * instantiate any variables that your implementation requires here. See
-     * recitation and lecture notes for guidance.
-     * <p>
-     * If an IOException is thrown by the BufferedReader, then hasNext should
-     * return false.
-     * <p>
-     * The only method that should be called on BufferedReader is readLine() and
-     * close(). You cannot call any other methods.
+     * Creates a FileLineIterator for the reader. 
      *
      * @param reader - A reader to be turned to an Iterator
      * @throws IllegalArgumentException if reader is null
@@ -59,9 +40,6 @@ public class FileLineIterator implements Iterator<String> {
     /**
      * Creates a FileLineIterator from a provided filePath by creating a
      * FileReader and BufferedReader for the file.
-     * <p>
-     * DO NOT MODIFY THIS METHOD.
-     * 
      * @param filePath - a string representing the file
      * @throws IllegalArgumentException if filePath is null or if the file
      *                                  doesn't exist
@@ -72,8 +50,6 @@ public class FileLineIterator implements Iterator<String> {
 
     /**
      * Takes in a filename and creates a BufferedReader.
-     * See Java's documentation for BufferedReader to learn how to construct one
-     * given a path to a file.
      *
      * @param filePath - the path to the CSV file to be turned to a
      *                 BufferedReader
@@ -87,7 +63,7 @@ public class FileLineIterator implements Iterator<String> {
         }
         try {
             FileReader fileReader = new FileReader(filePath);
-            return new BufferedReader(fileReader); // Complete this method.
+            return new BufferedReader(fileReader);   
         } catch (IOException e) {
             throw new IllegalArgumentException();
         }
@@ -96,10 +72,6 @@ public class FileLineIterator implements Iterator<String> {
     /**
      * Returns true if there are lines left to read in the file, and false
      * otherwise.
-     * <p>
-     * If there are no more lines left, this method attempts to close the
-     * BufferedReader. In case of an IOException during the closing process,
-     * an error message is printed to the console indicating the issue.
      *
      * @return a boolean indicating whether the FileLineIterator can produce
      *         another line from the file
@@ -113,18 +85,12 @@ public class FileLineIterator implements Iterator<String> {
                 System.out.println("IOException during the closing process"); // UNSURE
             }
         }
-        return next; // Complete this method.
+        return next;   
     }
 
     /**
      * Returns the next line from the file, or throws a NoSuchElementException
      * if there are no more strings left to return (i.e. hasNext() is false).
-     * <p>
-     * This method also advances the iterator in preparation for another
-     * invocation. If an IOException is thrown during a next() call, your
-     * iterator should make note of this such that future calls of hasNext()
-     * will return false and future calls of next() will throw a
-     * NoSuchElementException
      *
      * @return the next line in the file
      * @throws java.util.NoSuchElementException if there is no more data in the
@@ -146,6 +112,6 @@ public class FileLineIterator implements Iterator<String> {
         } else {
             throw new NoSuchElementException();
         } 
-        // Complete this method.
+ 
     }
 }
