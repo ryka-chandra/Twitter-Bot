e1b2ceee38eca671ec189d41d6f1c3fa3f958981
Ryka Chandra <147470636+ryka-chandra@users.noreply.github.com>
2024-08-12 20:53:47 -0400

    Update FileLineIteratorTest.java

--- FileLineIteratorTest.java
+++ FileLineIteratorTest.java
@@ -8,17 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /** Tests for FileLineIterator */
 public class FileLineIteratorTest {
-
-    /*
-     * Here's a test to help you out, but you still need to write your own.
-     */
-
     @Test
     public void testHasNextAndNext() {
-
-        // Note we don't need to create a new file here in order to test out our
-        // FileLineIterator if we do not want to. We can just create a
-        // StringReader to make testing easy!
         String words = "0, The end should come here.\n"
                 + "1, This comes from data with no duplicate words!";
         StringReader sr = new StringReader(words);
@@ -31,8 +22,7 @@ public class FileLineIteratorTest {
         assertFalse(li.hasNext());
     }
 
-    /* **** ****** **** WRITE YOUR TESTS BELOW THIS LINE **** ****** **** */
-
+ 
     @Test
     public void testEmpty() {
         StringReader sr = new StringReader("");
